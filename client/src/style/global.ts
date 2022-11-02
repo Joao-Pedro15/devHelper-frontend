aaac8910a2b1cb6849896cdd4f4e1346a76eef34
João Pedro <joao.monteiro@veredaeducacao.com>
2022-11-02 17:02:27 -0300

    style: create a global style with a styled-components

---
+++ client/src/style/global.ts
@@ -0,0 +1,33 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  * {
+    padding: 0;
+    margin: 0;
+    font-family: Verdana, Geneva, Tahoma, sans-serif;
+  }
+
+  html, body {
+    width: 100vw;
+    height: 100vh;
+  }
+
+  #root {
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
+
+  img {
+    margin: 0;
+  }
+
+  ul {
+    list-style: none;
+  }
+
+  a {
+    text-decoration: none;
+    color: #214392;
+  }
+`
