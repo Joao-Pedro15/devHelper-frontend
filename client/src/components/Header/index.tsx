be9f051812cc8edc9d616059265243a3c4e90496
João Pedro <joao.monteiro@veredaeducacao.com>
2022-11-02 19:54:51 -0300

    feat: create a header component

---
+++ client/src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import * as S from './style'
+
+export function Header() {
+  return (
+    <S.Container>
+      <div>Logo</div>
+      <div>Quem ta logado</div>
+    </S.Container>
+  )
+}
---
+++ client/src/components/Header/style.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components'
+
+export const Container = styled.header`
+  width: 100vw;
+  padding: 5px 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`
