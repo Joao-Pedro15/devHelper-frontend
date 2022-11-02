258d53c8f2c20bc7255229194dc47b9b35c2c0cb
João Pedro <joao.monteiro@veredaeducacao.com>
2022-11-02 19:52:17 -0300

    feat: create a login page

--- client/src/pages/Login/index.tsx
+++ client/src/pages/Login/index.tsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { ChangeEvent, FormEvent, useState, useContext } from 'react'
+import { useHistory } from 'react-router-dom'
+import { AuthContext } from '../../context/AuthContext'
 import * as S from './style'
 
+interface ILogin {
+  email: string,
+  password: string
+}
+
 export function Login() {
+  const history = useHistory()
+  const { userLogged, setUserLogged } = useContext(AuthContext)
+  const [form, setForm] = useState<null | ILogin>(null)
+
+  const handleInput = (e:ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target
+    setForm((prev:any) => ({
+      ...prev,
+      [name]: value
+    }))
+  } 
+
+  const LoginFn = (e:FormEvent) => {
+    e.preventDefault()
+    localStorage.setItem('_user', JSON.stringify(form))
+    setUserLogged(form)
+    window.location.href = './home'
+
+  }
+
   return (
     <S.Container>
-      <h2>Login</h2>      
+      <h1>Dev<span>Helper</span></h1>
+      <form onSubmit={(e:FormEvent) => LoginFn(e)}>
+        <section>
+          <label htmlFor="email">Email</label>
+          <input name="email" type="email" placeholder="Ex: example@gmail.com" onChange={handleInput} />
+        </section>
+        <section>
+          <label htmlFor="password">Senha</label>
+          <input name="password" type="password" placeholder="Digite sua senha" onChange={handleInput} />
+        </section>
+        <button type="submit">Login</button>
+      </form>
     </S.Container>
   )
 }  
--- client/src/pages/Login/style.ts
+++ client/src/pages/Login/style.ts
@@ -1,5 +1,65 @@
 import styled from 'styled-components'
 
 export const Container = styled.div`
-  color: red;
+  position: absolute;
+  margin: auto;
+  border: 1px solid #333;
+  border-radius: 10px;
+  width: 40%;
+  height: 50%;
+  left: 30%;
+  top: 20%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  justify-content: center;
+  box-shadow: 0px 2px 10px #eee;
+
+  h1{
+    color: darkred;
+    span { color: black }
+    border-bottom: 2px solid #ccc;
+  }
+
+  form {
+    width: 90%;
+    padding: 10px;
+    display: flex;
+    justify-content: space-evenly;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 20px;
+  }
+
+  section {
+    display: flex;
+    flex-direction: column;
+    gap: 5px;
+    position: relative;
+    width: 100%;
+    label {
+      font-weight: 700;
+      font-size:.8rem;
+      padding: 2px 5px;
+    }
+
+    input {
+      padding: 8px 10px;
+      outline: none;
+      border: 1px solid #333;
+      box-shadow: 0px 3px 10px #ccc;
+      border-radius: 2px;
+      color: #333;
+    }
+  }
+  button{
+    padding: 10px 20px;
+    outline: none;
+    font-weight: bold;
+    border-radius: 5px;
+    border: none;
+    background-color: darkred;
+    color: #eee;
+  }
 `
