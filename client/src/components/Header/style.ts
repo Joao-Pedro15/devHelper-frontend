adf763b1c2c75beb52b1886b7e82bb30d263aec2
Joao Monteiro <joao.monteiro@veredaeducacao.com>
2022-11-06 20:49:45 -0300

    style: change style padding and background color header

--- client/src/components/Header/style.ts
+++ client/src/components/Header/style.ts
@@ -2,8 +2,10 @@ import styled from 'styled-components'
 
 export const Container = styled.header`
   width: 100vw;
-  padding: 5px 10px;
+  padding: 15px 20px;
   display: flex;
   align-items: center;
   justify-content: space-between;
+  background-color: #333;
+  color: #fff;
 `
