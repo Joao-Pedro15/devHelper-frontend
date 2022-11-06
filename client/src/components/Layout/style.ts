cc8aab3679102ce4af61b39e9ec80ee3e29d1512
Joao Monteiro <joao.monteiro@veredaeducacao.com>
2022-11-06 20:52:18 -0300

    feat: implements toggle menu open or close logical

---
+++ client/src/components/Layout/style.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+
+type IProps = {
+  isToggle: boolean
+}
+
+const toggle = `
+"a1 a1 a1"
+"a2 a3 a3"
+"a2 a3 a3"
+`
+const notToggle = `
+"a1 a1 a1"
+"a3 a3 a3"
+"a3 a3 a3"
+`
+
+export const Container = styled.div<IProps>`
+  display: grid;
+  grid-template-areas: ${({isToggle}) => isToggle === true ? toggle : notToggle};
+
+
+  header {
+    grid-area: a1;
+  }
+
+  aside {
+    grid-area: a2;
+  }
+
+  main {
+    grid-area: a3;
+  }
+`
