ac70d871c51b164985ca47780df4ff76131be759
João Pedro <jjoao.monteiro15@gmail.com>
2022-11-07 23:15:06 -0300

    style: stylizing sidemenu

--- client/src/components/Sidebar/style.ts
+++ client/src/components/Sidebar/style.ts
@@ -1,5 +1,43 @@
 import styled from 'styled-components'
 
 export const Container = styled.aside`
-  background: blue;
+  border-top: 1px solid #222;
+  box-shadow: 2px 4px 4px #333;
+  background: #333;
+  height: 100vh;
+  display: flex;
+  align-items: flex-start;
+  justify-content: center;
+  max-width: 80%;
+
+  ul{
+    width:100%;
+    display: flex;
+    flex-direction: column;
+  }
+
+  a{
+    padding: 15px 0;
+    color: #FDFDFD;
+    display: grid;
+    place-items: center;
+    border-bottom: .5px solid #222;
+    transition: .1s ease-out;
+  }
+  a:hover{
+    background-color: #555;
+  }
+
+  li{
+    width: 90%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 4px;
+  }
+
+  li span:last-of-type{
+    font-size: 1.5rem;
+    margin-top: 5px;
+  }
 `
