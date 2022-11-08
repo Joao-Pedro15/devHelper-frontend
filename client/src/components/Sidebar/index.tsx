20f94b76a7cf1c39c954fe54da5518d56ee941f5
João Pedro <jjoao.monteiro15@gmail.com>
2022-11-07 23:14:23 -0300

    feat: mocking data items in hardCode and define structure

--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
 import * as S from './style'
+import {TbResize } from "react-icons/tb";
+import { SiMicrosoftexcel } from "react-icons/si";
+import { VscJson, VscFilePdf } from "react-icons/vsc";
+import { Link } from 'react-router-dom';
+
+type Items = {
+  name: string;
+  link: string;
+  icon: any
+}
 
 export function Sidebar() {
+    const items : Items[] = [
+      { name: 'Redimencionar', icon: <TbResize/>, link: '/' },
+      { name: 'Excel', icon: <SiMicrosoftexcel/>, link: '/' },
+      { name: 'Json', icon: <VscJson/>, link: '/' },
+      { name: 'PDF', icon: <VscFilePdf/>, link: '/' },
+  ]
   return (
     <S.Container>
-      <h4>sidebar</h4>
+      <ul>
+        { items.map((item) => (
+          <Link to={item.link}>
+            <li>
+              <span>{item.name}</span>
+              <span>{item.icon}</span>
+            </li>
+          </Link>
+        )) }
+      </ul>
     </S.Container>
   )
 }
