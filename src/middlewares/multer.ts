198a4f9cfcaed6ad28fec68ade3ee5ed784163e8
João Pedro <jjoao.monteiro15@gmail.com>
2022-11-12 23:02:54 -0300

    feat: create a middleware route from manipulation files

---
+++ src/middlewares/multer.ts
@@ -0,0 +1,37 @@
+import multer from 'multer'
+import { resolve } from 'path'
+import { randomBytes } from 'crypto'
+import { Request } from 'express'
+
+export const MulterConfig = {
+    dest: resolve(__dirname, '../', 'uploads'),
+    storage: multer.diskStorage({
+        destination: (request, file, callback)  =>  {
+            callback(null,  resolve(__dirname, '../', 'uploads'))
+        },
+        filename: (request, file, callback) => {
+            randomBytes(16, (err:Error | null, hash:Buffer) => {
+                if(err) return err
+                const fileName = `${hash.toString('hex')}-${file.originalname}`
+                callback(null, fileName)
+            })
+        },
+    }),
+    limits: {
+        fileSize: 2 * 1024 * 1024
+    },
+    fileFilter: (request: Request, file:Express.Multer.File, callback:(Error:Error | null | any , isSuccess: boolean) => void) => {
+        const allowedMimes = [
+            'image/jpeg',
+            'image/pjpeg',
+            'image/png',
+            'image/gif',
+        ]
+        if(allowedMimes.includes(file.mimetype)){
+            callback(null, true)
+        }else {
+            callback(new Error('Invalid file type.'), false)
+        }
+    },
+}
+
