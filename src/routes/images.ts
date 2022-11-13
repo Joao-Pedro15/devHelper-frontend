e8df0810f79e00b74911a8282ced7a46a86d23a2
João Pedro <jjoao.monteiro15@gmail.com>
2022-11-12 23:09:16 -0300

    feat: create a route with a multer post

--- src/routes/images.ts
+++ src/routes/images.ts
@@ -1,9 +1,21 @@
 import { Request, Response, Router } from 'express'
 const router = Router()
 
+import multer from 'multer'
+import { MulterConfig } from '../middlewares/multer'
+
+
 router.get('/', async (request: Request, response: Response) => {
     try {
         return response.send('hello wolrd')
+    } catch (error) {
+        return response.status(500).json(error)
+    }
+})
+
+router.post('/resize', multer(MulterConfig).single('file'), async (request: Request, response: Response) => {
+    try {
+        return response.status(201).json({message: 'file manipulation'})
     } catch (error) {
         return response.status(500).json(error)
     }
