efad560190437cab93db4e89a22a6dc6fe8e3826
João Pedro <jjoao.monteiro15@gmail.com>
2022-11-27 00:42:23 -0300

    feat: create a router from get all users

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,7 +1,35 @@
 import { Router, Request, Response } from 'express'
+import { authMiddleware } from '../middlewares/auth'
 
 const router = Router()
 
-router.post('/user')
+type ILogin =  {
+  username: string
+  password: string
+}
+
+router.post('/user', async (request: Request, response: Response) => {
+  const { username, password } : ILogin = request.body
+  try {
+    if(!username.trim() || !password.trim()) throw new Error('username and password not empty!')
+
+
+  } catch (error:any) {
+    return response.status(error.statusCode).json({message: error.message})
+  }
+})
+
+router.get('/allUsers', authMiddleware, async (request: Request, response: Response) => {
+  const users = [
+    {username: 'alonedw', password: 'nonpassword'},
+    {username: 'rockabillyboy', password: 'loremIpsum'}
+  ]
+  try {
+    return response.status(200).json(users)
+  } catch (error:any) {
+    return response.status(error.statusCode).json({message: error.message})
+  }
+})
+
 
 export default router
