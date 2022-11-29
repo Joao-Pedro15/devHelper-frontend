2afde4f446f3525c4615b4e458d296ee47fefc54
Joao Monteiro <joao.monteiro@veredaeducacao.com>
2022-11-29 13:18:11 -0300

    style: change style svg and button in header

--- client/src/components/Header/style.ts
+++ client/src/components/Header/style.ts
@@ -3,12 +3,25 @@ import styled from 'styled-components'
 export const Container = styled.header`
   position: fixed;
   width: 100%;
-  margin-bottom: 100px;
   background-color: var(--primary);  
+  z-index: 100;
+  color: #fff;
+  svg {
+    color: #fff;
+    border-color: #fff;
+  }
   div:first-of-type {
-    padding: .3rem 1rem;
+    color: #fff;
+    padding: .2rem 1rem;
     display: flex;
     align-items: center;
     justify-content: space-between;
+  }
+
+  button {
+    background-color: transparent;
+    svg { font-size: 1.5rem };
+    margin-left: -15px;
+    border: none;
   }
 `
