7313d8d77817a8faa09d8763ccfe2a0c7d3afca8
João Pedro <joao.monteiro@veredaeducacao.com>
2022-11-30 12:01:40 -0300

    feat: create a functionality resize image integration back end front end

--- client/src/pages/Resize/index.tsx
+++ client/src/pages/Resize/index.tsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { FormEvent, useState } from 'react'
 import { Layout } from '../../components/Layout'
+import { API } from '../../services/api'
 import * as S from './style'
 
 export function Resize() {
+  const [image, setImage] : any = useState()
+
+  const onSubmit = async(e:FormEvent) => {
+    e.preventDefault()
+    const data = new FormData()
+    data.append('file', image)
+    
+    await API.post('/images/resize?width=300', data)
+    
+    console.log('opa opa opa em ');
+    
+  }
   return (
     <Layout>
       <S.Container>
-        <h1>Opa</h1>
+        <h1>Redimensionamento</h1>
+        <form onSubmit={onSubmit}>
+          <input type="file" onChange={(e:any) => setImage(e.target.files[0])} />
+          <button type='submit'>Redimensionar</button>
+        </form>
       </S.Container>
     </Layout>    
   )
