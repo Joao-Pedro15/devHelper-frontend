9e7bc5ce7e35b4eaf46d8362a5f19c73352ef0c7
João Pedro <joao.monteiro@veredaeducacao.com>
2022-12-01 17:17:40 -0300

    feat: create a testing route change file extension image

--- src/routes/images.ts
+++ src/routes/images.ts
@@ -4,6 +4,9 @@ const router = Router()
 import multer from 'multer'
 import { MulterConfig } from '../middlewares/multer'
 import { resizeImage } from '../middlewares/sharp'
+import { changeExtension } from '../middlewares/jimp'
+import jimp from 'jimp'
+import { join } from 'path'
 
 router.get('/', async (request: Request, response: Response) => {
     try {
@@ -15,4 +18,14 @@ router.get('/', async (request: Request, response: Response) => {
 
 router.post('/resize', multer(MulterConfig).single('file'), resizeImage)
 
+router.post('/changeExtension', (request: Request, response: Response) => {
+    const input = join(__dirname, '..', 'uploads', 'maykao.jpg')
+  const output = join(__dirname, '..', 'images', 'maykao.pdf')
+  
+  jimp.read(input, function(error, image){
+    if(error) return response.status(500).send(error.message)
+    image.write(output)
+    return response.status(200).send("deu certo!")
+  })
+})
 export default router
