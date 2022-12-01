8b813811735efb8490beafae0ee511215a9ef90a
João Pedro <joao.monteiro@veredaeducacao.com>
2022-12-01 17:31:52 -0300

    feat: create a page by convert images extension

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -4,6 +4,7 @@ import { Route } from "./route";
 import { Login } from '../pages/Login'
 import { Home } from '../pages/Home'
 import { Resize } from '../pages/Resize'
+import { ConvertImage } from '../pages/ConvertImage'
 
 const Routes: React.FC = () => {
   return (  
@@ -14,6 +15,8 @@ const Routes: React.FC = () => {
       <Route isPrivate path="/home" component={Home} />
 
       <Route isPrivate path={"/module/resize"} component={Resize} />
+
+      <Route isPrivate path={"/module/convertImage"} component={ConvertImage} />
 
       {/* ==================== ERRO ==================== */}
       {/* <Route component={NotFoundPage} /> */}
