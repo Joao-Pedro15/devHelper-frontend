4907586390fb0c49f0fd2438d5a28db400ef4fdd
João Pedro <joao.monteiro@veredaeducacao.com>
2022-12-02 10:37:39 -0300

    chore: rename middleware and changes your params (convertImage)

--- src/middlewares/jimp.ts
+++ src/middlewares/jimp.ts
@@ -1,11 +1,11 @@
 
-import { Response, Request } from 'express'
+import { Response, Request, NextFunction } from 'express'
 import jimp from 'jimp'
 import { join } from 'path'
 
-export function changeExtension(response: Response) {
+export function convertImage(request: Request, response: Response, next: NextFunction) {
   const input = join(__dirname, '..', 'uploads', 'maykao.jpg')
-  const output = join(__dirname, '..', 'uploads', 'maykao.png')
+  const output = join(__dirname, '..', 'images', 'maykao.png')
   
   jimp.read(input, function(error, image){
     if(error) return response.status(500).send(error.message)
