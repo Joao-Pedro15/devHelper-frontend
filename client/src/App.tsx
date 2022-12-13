9a4aa59e4ff873c8c56076ac083a860b6f0263e1
João Pedro <joao.monteiro@veredaeducacao.com>
2022-12-13 11:50:16 -0300

    chore: change provider routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Routes } from './routes'
 import { GlobalStyle } from './style/global'
 import { BrowserRouter } from 'react-router-dom'
-import { AuthProvider } from './context/AuthContext'
+// import { AuthProvider } from './context/AuthContext'
+import { AuthProvider } from './auth/AuthProvider'
 
 function App() {
   return (
